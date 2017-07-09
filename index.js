e869a062da6f1ad1afc23bf4a9d5ba94cda80b65
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-07-09 21:24:40 +0530

    cli-color package added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Tuneshuffle app server code
 
 const express = require('express'),
+	color = require('cli-color'),
 	app = express(),
 	constants = require('./lib/constants');
 
 app.listen(constants.PORT, () => {
-	console.log(`${constants.APP_NAME} is running on port : ${constants.PORT}`);
+	console.log(`${color.blue(constants.APP_NAME)} is running on port : ${color.yellow(constants.PORT)}`);
 });
