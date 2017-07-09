43c588e766260ce68a3c87b35aeb9665386fa4e2
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-07-09 22:34:16 +0530

    Express static view templating setup added

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 
 const express = require('express'),
 	color = require('cli-color'),
-	app = express(),
-	constants = require('./lib/constants');
+	constants = require('./lib/constants'),
+	path = require('path'),
+	app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function (req, res) {
+  return res.render('index');
+})
 
 app.listen(constants.PORT, () => {
 	console.log(`${color.blue(constants.APP_NAME)} is running on port : ${color.yellow(constants.PORT)}`);
