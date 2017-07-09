d6ff7512046416e9e13c1638ae219f9a9eb88fa3
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-07-09 22:50:35 +0530

    Constants added as app locals - to be used in express views

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const express = require('express'),
 	path = require('path'),
 	app = express();
 
+// Setting public assets directory and view engine
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function (req, res) {
-  return res.render('index');
-})
+// Setting app locals
+app.locals.constants = constants;
 
+// Setting default route
+app.get('/', (req, res) => res.render('index'));
+
+// Starting app server on specified port
 app.listen(constants.PORT, () => {
-	console.log(`${color.blue(constants.APP_NAME)} is running on port : ${color.yellow(constants.PORT)}`);
+	console.log(`${color.blue(constants.APP_TITLE)} is running on port : ${color.yellow(constants.PORT)}`);
 });
