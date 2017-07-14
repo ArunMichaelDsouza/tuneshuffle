01c6121147efc039a2c33935f1c918243f62d284
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-07-15 04:05:18 +0530

    Datastore module added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Tuneshuffle app server code
 
 const express = require('express'),
-	color = require('cli-color'),
-	constants = require('./lib/constants'),
-	path = require('path'),
-	app = express();
+    color = require('cli-color'),
+    constants = require('./lib/constants'),
+    path = require('path'),
+    app = express();
 
 // Setting public assets directory and view engine
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,5 +19,5 @@ app.get('/', (req, res) => res.render('index'));
 
 // Starting app server on specified port
 app.listen(constants.PORT, () => {
-	console.log(`${color.blue(constants.APP_TITLE)} is running on port : ${color.yellow(constants.PORT)}`);
+    console.log(`${color.blue(constants.APP_TITLE)} is running on port : ${color.yellow(constants.PORT)}`);
 });
