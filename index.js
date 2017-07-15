21d2ba8fd036ac6ced64d9ffd3c180c4b94b9980
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-07-16 00:29:36 +0530

    Woodlot logger added

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ const express = require('express'),
     constants = require('./lib/constants'),
     path = require('path'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    woodlot = require('woodlot').middlewareLogger,
+    cookieParser = require('cookie-parser');
+
+// Enable body/cookie parser middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Logger middleware
+app.use(woodlot({
+    streams: ['./logs/tuneshuffle-server.log'],
+    stdout: true,
+    format: {
+        options: {
+            cookies: true,
+            headers: true
+        }
+    }
+}));
 
 // Enable api module
 require('./lib/api')(app);
-
-// Setup body parser for express requests
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setup public assets directory and view engine
 app.use(express.static(path.join(__dirname, 'public')));
@@ -24,7 +39,7 @@ app.locals.constants = constants;
 // Setup default route
 app.get('/', (req, res) => res.render('index'));
 
-// Setup app server on specified port
+// Run app server on specified port
 app.listen(constants.PORT, () => {
     console.log(`${color.blue(constants.APP_TITLE)} is running on port : ${color.yellow(constants.PORT)}`);
 });
