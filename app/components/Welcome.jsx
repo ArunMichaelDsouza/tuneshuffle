898b3fe4f2a89aaaf1311513037f79aa7ff0d0a5
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-07-17 01:51:59 +0530

    Sass button component styles added

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -8,6 +8,9 @@ const Welcome = () => {
 			<div className="card-info">
 				Your offline solution to pair music streaming! Start a server, ask your friends to join in. Stream and share music together.
 			</div>
+			<div className="text-center">
+				<button className="btn-primary-line btn-start">Start Streaming</button>
+			</div>
 		</section>
 	);
 };
